cd9d678bbdbbe1f8b259927f69af148a78603bbe
Anton Kovsh <kovsh_anton@ukr.net>
2023-01-18 13:47:27 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/';
+const KEY = '7b0e471f76e5da9e6415f6c271770eca';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  // https://api.themoviedb.org/3/movie/550?api_key=7b0e471f76e5da9e6415f6c271770eca
+  const fetchApi = async () => {
+    try {
+      const resolved = await axios.get(
+        `${BASE_URL}/3/movie/550?api_key=${KEY}`
+      );
+      console.log(resolved);
+    } catch (error) {}
+  };
+
+  return <div>Started</div>;
 };
