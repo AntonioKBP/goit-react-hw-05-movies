4bd498d7ac290bf6534f2d371601e7151c9afef4
Anton Kovsh <kovsh_anton@ukr.net>
2023-01-20 13:44:48 +0200

    upd

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,107 +1,48 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const BASE_URL = 'https://api.themoviedb.org/';
 const KEY = '7b0e471f76e5da9e6415f6c271770eca';
 
 const Home = () => {
-    const [filmHits, setFilmHits] = useState([]);
-    const [film, setFilm] = useState([]);
-    
+  const [searchReq, setSearchReq] = useState('');
+  const [filmHits, setFilmHits] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        const fetchData = async() = {
-      
-        }
-    },[])
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      // setIsLoading(true);
+      try {
+        const { data } = await axios.get(
+          `https://api.themoviedb.org/3/trending/movie/day?api_key=7b0e471f76e5da9e6415f6c271770eca`
+        );
+        setMovies(prev => [...prev, ...data.results]);
+        console.log(data);
+        // setImageHits(data);
+        // if (page === 1) {
+        //   toast.success(`We found ${data.total} images`);
+        // }
+      } catch (error) {
+        setMovies([]);
+        // toast.error('Cannot process your request');
+      } finally {
+        // setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <ul>
-      <li></li>
+      {movies.map(movie => (
+        <li key={movie.id}>
+          <Link>{movie.title}</Link>
+        </li>
+      ))}
     </ul>
   );
 };
 
 export default Home;
-
-// export const App = () => {
-//   const [image, setImage] = useState([]);
-//   const [search, setSearch] = useState('');
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [page, setPage] = useState(1);
-//   const [imageHits, setImageHits] = useState([]);
-//   const [showModal, setShowModal] = useState(false);
-//   const [url, setUrl] = useState('');
-//   const [alt, setAlt] = useState('');
-
-//   const handleSearch = search => {
-//     setSearch(search);
-//     setPage(1);
-//     setImage([]);
-//   };
-
-//   const loadMore = () => {
-//     setPage(prev => prev + 1);
-//   };
-
-//   useEffect(() => {
-//     if (!search) {
-//       return;
-//     }
-
-//     const fetchData = async () => {
-//       setIsLoading(true);
-//       try {
-//         const { data } = await axios.get(
-//           `${BASE_URL}q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//         );
-
-//         setImage(prev => [...prev, ...data.hits]);
-//         setImageHits(data);
-
-//         if (page === 1) {
-//           toast.success(`We found ${data.total} images`);
-//         }
-//       } catch (error) {
-//         setImage([]);
-//         toast.error('Cannot process your request');
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-//     fetchData();
-//   }, [search, page]);
-
-//   const toggleModal = () => {
-//     setShowModal(pS => !pS);
-//   };
-
-//   const handleModal = (url, alt) => {
-//     toggleModal();
-
-//     setUrl(url);
-//     setAlt(alt);
-//   };
-
-//   return (
-//     <Container>
-//       <SearchBar onSubmit={handleSearch} />
-
-//       {
-//         <ImageGallery>
-//           {<ImageGalleryItem image={image} onhandleModal={handleModal} />}
-//         </ImageGallery>
-//       }
-//       {isLoading && <Loader />}
-//       {<Toast />}
-//       {showModal && (
-//         <Modal onClose={toggleModal}>
-//           <ModalInner url={url} alt={alt} />
-//         </Modal>
-//       )}
-//       {image.length === 0 || imageHits.totalHits === image.length || (
-//         <Button onClick={loadMore} />
-//       )}
-//     </Container>
-//   );
-// };
