d57d5f3fe93a4cb5bc7721aefcbbdbbb1a7ef2d2
AntonioKBP <105855258+AntonioKBP@users.noreply.github.com>
2023-01-26 12:52:10 +0000

    Deploying to gh-pages from @ AntonioKBP/goit-react-hw-05-movies@bac87fba9d6c1ea22a003976183b380e7b86d233 🚀

---
+++ static/js/38.7cea895c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[38],{38:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"Page Not Found 404"})}}}]);
+//# sourceMappingURL=38.7cea895c.chunk.js.map
